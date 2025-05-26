c8bbdd5812ebe9b15a6eede249c4cc913431c911
KEEKE132 <dksvlfdhs1@gmail.com>
2025-05-26 15:48:36 +0900

    refactor: Board 엔티티 설계 수정

--- backend/src/main/java/com/nuriwoolim/pagebackend/entity/Board.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/entity/Board.java
@@ -9,31 +9,36 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
-
+import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    @Column(unique = true, length = 20)
+    @Column(unique = true, length = 20, nullable = false)
     private String title;
 
     @Column(length = 100)
     private String description;
 
-    @NotBlank
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
     private BoardType type = BoardType.OTHER;
 
-    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY)
-    private List<Post> postList;
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, orphanRemoval = true,
+            cascade = {jakarta.persistence.CascadeType.REMOVE})
+    @Builder.Default
+    private List<Post> postList = new ArrayList<>();
 }
