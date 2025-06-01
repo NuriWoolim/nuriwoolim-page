799d4974f5d7dbb54698d375bc4a913b4bed3c14
Benzity <benzity@naver.com>
2025-06-01 23:43:28 +0900

    초기 커밋, frontend react project 추가.

--- frontend/src/components/Notice.jsx
+++ frontend/src/components/Notice.jsx
@@ -2,29 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-// Memo : 먼가 동연회 공지, 누리울림 자체 공지, 학교 공지 등등을 구별하는 색깔이 왼쪽에 뜨면 좋을 것 같음.
-
-const Notices = [
-  { id: 1, title: "누리울림 봄철 합주회 뱃지", category: "누리울림" },
-  {
-    id: 2,
-    title: "본무대 시 동아리방 이용 관련 공지",
-    category: "동아리연합회",
-  },
-  {
-    id: 3,
-    title: "LUCAUS 축제 본무대로 인한 107관 출입 통제 안내",
-    category: "중앙대학교",
-  },
-  { id: 4, title: "공용 기타 이펙터 프리셋 시트 공지", category: "누리울림" },
-  { id: 5, title: "악기 & 장비 사용 리마인드 공지", category: "누리울림" },
-  { id: 6, title: "드럼 킥드럼 피 공지", category: "누리울림" },
-];
-
+/* Notice 섹션의 전체 배경 */
 const NoticeSection = styled.section`
   background-color: #fefaef;
   padding: 45px 0;
 `;
+
+/* Notice 섹션의 컨테이너 박스 */
 
 const NoticeWrapper = styled.div`
   width: 90%;
@@ -56,6 +40,26 @@ const NoticeWrapper = styled.div`
     padding: 0;
   }
 `;
+
+/* Notice 섹션의 Notice 항목들. 클릭 시 해당 문서로 Redirect. */
+
+const Notices = [
+  { id: 1, title: "누리울림 봄철 합주회 뱃지", category: "누리울림" },
+  {
+    id: 2,
+    title: "본무대 시 동아리방 이용 관련 공지",
+    category: "동아리연합회",
+  },
+  {
+    id: 3,
+    title: "LUCAUS 축제 본무대로 인한 107관 출입 통제 안내",
+    category: "중앙대학교",
+  },
+  { id: 4, title: "공용 기타 이펙터 프리셋 시트 공지", category: "누리울림" },
+  { id: 5, title: "악기 & 장비 사용 리마인드 공지", category: "누리울림" },
+  { id: 6, title: "드럼 킥드럼 피 공지", category: "누리울림" },
+];
+
 
 const Notice = () => {
   return (
