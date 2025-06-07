ca6a7c1ca6c7ce8c73ca7843d949c7a5b4ab6593
KEEKE132 <dksvlfdhs1@gmail.com>
2025-06-07 18:13:05 +0900

    feat: EntryPoint 추가

--- backend/src/main/java/com/nuriwoolim/pagebackend/core/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/core/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.nuriwoolim.pagebackend.core.jwt;
 
 import com.nuriwoolim.pagebackend.core.jwt.dto.TokenBody;
+import com.nuriwoolim.pagebackend.core.security.CustomEntryPoint;
 import com.nuriwoolim.pagebackend.core.security.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,7 +10,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -20,24 +23,34 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final com.nuriwoolim.pagebackend.core.jwt.JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
     private final CustomUserDetailsService userDetailsService;
+    private final CustomEntryPoint customEntryPoint;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String token = resolveToken(request);
-        log.debug("[JwtAuthenticationFilter] doFilterInternal = {}", "Enter Filter");
-        if (token != null && jwtTokenProvider.validate(token)) {
-            TokenBody tokenBody = jwtTokenProvider.parseJwt(token);
-            UserDetails userDetails = userDetailsService.loadUserById(tokenBody.id());
+        log.debug("Enter Filter: {}", token);
+        try {
+            if (token != null && jwtTokenProvider.validate(token)) {
+                TokenBody tokenBody = jwtTokenProvider.parseJwt(token);
+                UserDetails userDetails = userDetailsService.loadUserById(tokenBody.id());
+                log.debug("User: {}", userDetails);
 
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,
-                    null,
-                    userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.debug("SecurityContextHolder.getContext()={}", SecurityContextHolder.getContext());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                throw new BadCredentialsException("Invalid token");
+            }
+        } catch (AuthenticationException e) {
+            SecurityContextHolder.clearContext();
+            customEntryPoint.commence(request, response, e);
+            return;
         }
 
         filterChain.doFilter(request, response);
--- backend/src/main/java/com/nuriwoolim/pagebackend/core/security/SecurityConfig.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/core/security/SecurityConfig.java
@@ -1,30 +1,35 @@
 package com.nuriwoolim.pagebackend.core.security;
 
+import com.nuriwoolim.pagebackend.core.jwt.JwtAuthenticationFilter;
+import com.nuriwoolim.pagebackend.core.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder sharedObject = http.getSharedObject(AuthenticationManagerBuilder.class);
-        AuthenticationManager authenticationManager = sharedObject.build();
-
-        http.authenticationManager(authenticationManager);
-
-        http.formLogin(AbstractHttpConfigurer::disable);
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider,
+                                           CustomUserDetailsService customUserDetailsService) throws Exception {
+        http.formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
