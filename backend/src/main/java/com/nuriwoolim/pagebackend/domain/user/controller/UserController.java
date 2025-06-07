ebb91a44393b70c4d63462358921b7cd5b4fc777
KEEKE132 <dksvlfdhs1@gmail.com>
2025-06-07 19:15:34 +0900

    refactor: UserController URI 수정

--- backend/src/main/java/com/nuriwoolim/pagebackend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/domain/user/controller/UserController.java
@@ -1,48 +1,37 @@
 package com.nuriwoolim.pagebackend.domain.user.controller;
 
-import com.nuriwoolim.pagebackend.domain.user.dto.UserCreateRequest;
 import com.nuriwoolim.pagebackend.domain.user.dto.UserResponse;
 import com.nuriwoolim.pagebackend.domain.user.dto.UserUpdateRequest;
 import com.nuriwoolim.pagebackend.domain.user.service.UserService;
 import jakarta.validation.Valid;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Long userId) {
         return ResponseEntity.ok(userService.findById(userId));
     }
 
-    @PostMapping("/users")
-    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserCreateRequest userCreateRequest) {
-        UserResponse response = userService.create(userCreateRequest);
-        URI location = URI.create("/api/users/" + response.id());
-        return ResponseEntity.created(location)
-                .body(response);
-    }
-
-    @PatchMapping("/users/{userId}")
+    @PatchMapping("/{userId}")
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long userId,
                                                    @Valid @RequestBody UserUpdateRequest userUpdateRequest) {
         return ResponseEntity.ok(userService.update(userId, userUpdateRequest));
     }
 
-    @DeleteMapping("/users/{userId}")
+    @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
         userService.delete(userId);
         return ResponseEntity.ok().build();
