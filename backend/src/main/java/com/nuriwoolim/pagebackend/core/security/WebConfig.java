861c0f1ef0644071383883f2a42b22ce0911d6b3
KEEKE132 <dksvlfdhs1@gmail.com>
2025-07-14 16:05:01 +0900

    fix: 헤더 노출 및, 쿠키 정책 Lax

--- backend/src/main/java/com/nuriwoolim/pagebackend/core/security/WebConfig.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/core/security/WebConfig.java
@@ -19,6 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
+            .exposedHeaders("Authorization")
             .allowCredentials(true)
             .maxAge(3600);
     }
