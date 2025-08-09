81352d59f7210cb9fb5a8388ea73c2561c07b768
KEEKE132 <dksvlfdhs1@gmail.com>
2025-08-09 21:24:48 +0900

    chore: 주석 제거

--- backend/src/main/java/com/nuriwoolim/pagebackend/domain/timeTable/util/TimeTableMapper.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/domain/timeTable/util/TimeTableMapper.java
@@ -1,34 +1,34 @@
-//package com.nuriwoolim.pagebackend.domain.timeTable.util;
-//
-//import com.nuriwoolim.pagebackend.domain.timeTable.dto.TimeTableCreateRequest;
-//import com.nuriwoolim.pagebackend.domain.timeTable.dto.TimeTableResponse;
-//import com.nuriwoolim.pagebackend.domain.timeTable.entity.TimeTable;
-//import com.nuriwoolim.pagebackend.domain.user.entity.User;
-//import lombok.AccessLevel;
-//import lombok.NoArgsConstructor;
-//
-//@NoArgsConstructor(access = AccessLevel.PRIVATE)
-//public class TimeTableMapper {
-//
-//    public static TimeTable fromTimeTableCreateRequest(TimeTableCreateRequest request, User user) {
-//        return TimeTable.builder()
-//            .title(request.title())
-//            .description(request.description())
-//            .team(request.team())
-//            .start(request.start())
-//            .end(request.end())
-//            .user(user)
-//            .build();
-//    }
-//
-//    public static TimeTableResponse toTimeTableResponse(TimeTable timeTable) {
-//        return TimeTableResponse.builder()
-//            .id(timeTable.getId())
-//            .title(timeTable.getTitle())
-//            .description(timeTable.getDescription())
-//            .team(timeTable.getTeam())
-//            .start(timeTable.getStart())
-//            .end(timeTable.getEnd())
-//            .build();
-//    }
-//}
+package com.nuriwoolim.pagebackend.domain.timeTable.util;
+
+import com.nuriwoolim.pagebackend.domain.timeTable.dto.TimeTableCreateRequest;
+import com.nuriwoolim.pagebackend.domain.timeTable.dto.TimeTableResponse;
+import com.nuriwoolim.pagebackend.domain.timeTable.entity.TimeTable;
+import com.nuriwoolim.pagebackend.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TimeTableMapper {
+
+    public static TimeTable fromTimeTableCreateRequest(TimeTableCreateRequest request, User user) {
+        return TimeTable.builder()
+            .title(request.title())
+            .description(request.description())
+            .team(request.team())
+            .start(request.start())
+            .end(request.end())
+            .user(user)
+            .build();
+    }
+
+    public static TimeTableResponse toTimeTableResponse(TimeTable timeTable) {
+        return TimeTableResponse.builder()
+            .id(timeTable.getId())
+            .title(timeTable.getTitle())
+            .description(timeTable.getDescription())
+            .team(timeTable.getTeam())
+            .start(timeTable.getStart())
+            .end(timeTable.getEnd())
+            .build();
+    }
+}
