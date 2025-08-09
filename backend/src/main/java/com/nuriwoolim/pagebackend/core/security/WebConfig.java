b8059df65b22b948a40936c52f579da1e36e85ca
KEEKE132 <dksvlfdhs1@gmail.com>
2025-08-09 21:24:08 +0900

    chore: CORS 오리진 추가

--- backend/src/main/java/com/nuriwoolim/pagebackend/core/security/WebConfig.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/core/security/WebConfig.java
@@ -1,11 +1,16 @@
 package com.nuriwoolim.pagebackend.core.security;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("custom.cors.origins")
+    List<String> corsOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,9 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
                 "http://localhost:3000",
                 "http://localhost:8080",
                 "http://localhost:5500",
+                "http://localhost:5173",
                 "https://nuriwoolimtest.netlify.app",
                 "https://nuriwoolim.n-e.kr"
             )
+            .allowedOrigins(corsOrigins.toArray(new String[0]))
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("Authorization", "authorization")
