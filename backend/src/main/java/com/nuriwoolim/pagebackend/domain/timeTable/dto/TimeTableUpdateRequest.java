a6caeb7f34f6b6ac289cdf65a4ced261db25985b
KEEKE132 <dksvlfdhs1@gmail.com>
2025-08-10 19:04:57 +0900

    feat: update dto 수정

--- backend/src/main/java/com/nuriwoolim/pagebackend/domain/timeTable/dto/TimeTableUpdateRequest.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/domain/timeTable/dto/TimeTableUpdateRequest.java
@@ -1,21 +1,15 @@
 package com.nuriwoolim.pagebackend.domain.timeTable.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record TimeTableUpdateRequest(
-    @NotBlank
     String title,
     String description,
-    @NotBlank
     String team,
 
-    @NotNull
     LocalDateTime start,
-    @NotNull
     LocalDateTime end
 ) {
 
