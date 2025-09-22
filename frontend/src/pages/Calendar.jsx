1ff367d7ee76a950a236992bad97f4c602976cd3
Daehan Kang <gangdaehan@gmail.com>
2025-09-22 18:11:29 +0900

    Feat: Timetable (#61)
    * Initial commit
    
    * 초기 커밋. Header, Notice 초안 완성.
    
    * 초기 커밋, frontend react project 추가.
    
    * Modified Nav Bar, added Footer Container
    
    * 로그인/회원가입 창 구현, NOTICE 창 디자인
    
    * axios 설치 반영
    
    * axios 설치 반영
    
    * 오타 수정
    
    * 안쓰는 모듈(import) 제거
    
    * 처음 접속 시 header 부분이 페이드인효과로 떠지게 설정
    
    * 로그인 창 구현, 로그인/회원가입 API 연동 성공
    
    * user.js authorization 소문자로 수정, Signup UI 구현
    
    * 사용하지 않은 변수 삭제
    
    * refreshToken local에 저장 안하게 만듦, 로그인창 토큰 만료 오류 수정
    
    * Archive 카테고리 구현 + 로그인 버튼 누를 시 hover되게
    
    * 로그인 창 최종 완성
    
    * feat: 로그인 시 마이페이지 버튼(드롭다운 포함)과 로그아웃 버튼이 이제 보임
    
    * fix: 오타 수정 및 README.md 내용 수정
    
    * feat: 사용자가 회원가입 버튼 누를 시 약관동의창 띄우기
    
    * refactoring: vite
    
    * docs: 이름 드롭다운 내용 수정
    
    * calendar started
    
    * feat: month change added
    
    * showing datas on dates
    
    * time selector on process
    
    * time selector done
    
    * seperated selector table.
    Using array instead of set for more functions
    
    * timetable colorfull
    
    * timetable infos on table
    
    * tt crud added
    
    * 타임테이블 일 단위 말고 한달 단위로 불러오기로 수정
    
    * feat: footer
    
    * footer upd
    
    * footer done
    
    * calendar css upd
    rem = 14px for mac air
    
    * 캘린더 안 작은 타임테이블 표시 css 업데이트
    
    * 타임테이블 수정 중간세이브
    
    * 날짜 상세창
    
    * 날짜 상세창 css 작업중
    
    * 소문자 오류 DateCell.jsx
    
    ---------
    
    Co-authored-by: Caleb Choi <102143206+Benzity@users.noreply.github.com>
    Co-authored-by: Benzity <benzity@naver.com>

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,52 +1,266 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import styled from "styled-components";
+import DateCell from "./calendar/DateCell";
+import { TimeTableAPI } from "../apis/common";
+import { createDate } from "../tools/DateTool";
 
-/* Notice 섹션의 전체 배경 */
-const NoticeSection = styled.section`
+
+/* Calendar 섹션의 전체 배경 */
+const CalendarSection = styled.section`
   background-color: #fefaef;
-  padding: 45px 0;
+  padding: 2.8rem 0;
 `;
 
-/* Notice 섹션의 컨테이너 박스 */
-
-const NoticeWrapper = styled.div`
-  width: 90%;
+/* Calendar 섹션의 컨테이너 박스 */
+const CalendarWrapper = styled.div`
+  width: 92%;
   margin: 0 auto;
-  padding: 0px 40px 40px;
+  padding: 0px 17rem 0 17rem;
+  box-sizing: border-box;
   background-color: #ffffff;
   border: 4px solid #033148;
 
-  h2 {
-    font-family: Plus Jakarta Sans;
-    font-weight: 800;
-    font-size: 40px;
-    line-height: 127%;
-    letter-spacing: -6%;
-    text-transform: uppercase;
-  }
-
-  p {
-    font-family: Pretendard;
-    font-weight: 500;
-    font-size: 30px;
-    line-height: 127%;
-    letter-spacing: -3%;
-    text-transform: uppercase;
-  }
-
-  ul {
+  > ul {
     list-style: none;
     padding: 0;
   }
 `;
 
+// 전체 테이블
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, calc(100% / 7));
+  grid-template-rows: 2.7rem repeat(6, 9rem);
+  width: 100%;
+
+  border-top: 1px solid #033148;
+  border-left: 1px solid #033148;
+`;
+
+// Days of Weeks의 셀 한 칸.
+const WeekDayCell = styled.div`
+  padding: 0.6rem;
+
+  border-right: 1px solid #033148;
+  border-bottom: 1px solid #033148;
+  background: #486284;
+  color: #fefaef;
+  font-family: Pretendard;
+  font-size: 1.1rem;
+  font-weight: 800;
+`;
+
+// 현재 년월 라벨과 버튼들을 감싸는 컨테이너
+const MonthYearContainer = styled.div`
+  display: flex;
+  width: 17rem;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1.2rem 0 1.2rem 0;
+  h2 {
+    color: #486284;
+
+    font-family: Pretendard;
+    font-size: 2.2rem;
+    font-weight: 900;
+    margin: 0;
+  }
+
+  button {
+    background: none; /* 배경 제거 */
+    width: 2rem;
+    border: none; /* 보더 제거 */
+    padding: 0; /* 기본 패딩 제거 */
+    cursor: pointer; /* 포인터 커서 */
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  button img{
+    height: 1.5rem;
+    margin-top: 5px;
+  }
+  button:hover img {
+    filter: brightness(0.8); /* 밝게 */
+  }
+  button:active img {
+    filter: brightness(0.6); /* 어둡게 */
+  }
+
+  .down {
+    transform: scaleX(-1);
+  }
+`;
+
+// 현재 달의 날짜들을 찾고 올바른 위치를 찾아주는 함수
+const createCalendarData = (startDate) => {
+  const dates = [];
+  const row = 6;
+  const col = 7;
+
+  // 현재 달의 마지막 날
+  const lastDate = new Date(
+    startDate.getFullYear(),
+    startDate.getMonth() + 1,
+    0
+  ).getDate();
+
+  // 이전 달의 마지막 날
+  const prevLastDate = new Date(
+    startDate.getFullYear(),
+    startDate.getMonth(),
+    0
+  ).getDate();
+
+  // 첫 번째 날의 요일 (0: 일요일, 1: 월요일, ...)
+  const firstDayOfWeek = (startDate.getDay() + 6) % 7;
+
+  // 이전 달 날짜들 추가
+  for (let i = firstDayOfWeek - 1; i >= 0; i--) {
+    dates.push(
+      new Date(
+        startDate.getFullYear(),
+        startDate.getMonth() - 1,
+        prevLastDate - i
+      )
+    );
+  }
+
+  // 현재 달 날짜들 추가
+  for (let date = 1; date <= lastDate; date++) {
+    dates.push(new Date(startDate.getFullYear(), startDate.getMonth(), date));
+  }
+
+  // 다음 달 날짜들 추가
+  const totalCells = row * col;
+  const remainingCells = totalCells - dates.length;
+  for (let date = 1; date <= remainingCells; date++) {
+    dates.push(
+      new Date(startDate.getFullYear(), startDate.getMonth() + 1, date)
+    );
+  }
+
+  return {
+    startDate: startDate,
+    dates: dates,
+  };
+};
+
 const Calendar = () => {
+  const row = 6;
+  const col = 7;
+  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const curDate = new Date();
+
+  const [calendarState, setCalendarState] = useState(() => {
+    const today = new Date();
+    const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+    return createCalendarData(startDate);
+  });
+
+  const getTimeTables = async () => {
+    try {
+      // 한달간 기간 설정
+      const result = await TimeTableAPI.getTimeTable();
+      const resultdata = result.data;
+
+      const newMonthTT = Array.from({ length: 42 }, () => []);
+      calendarState.dates.map((date, i) => {
+        resultdata.data.map((timetable) => {
+          const start = createDate(timetable.start);
+          const end = createDate(timetable.end);
+
+          if (
+            date.getDate() == start.getDate() &&
+            date.getMonth() == start.getMonth()
+          )
+            newMonthTT[i].push(timetable);
+
+          if (end.getDate() != start.getDate()) {
+            if (
+              date.getDate() == end.getDate() &&
+              date.getMonth() == end.getMonth()
+            )
+              newMonthTT[i].push(timetable);
+          }
+        });
+      });
+
+      setMonthTT(newMonthTT);
+    } catch (error) {
+      console.log("getTimeTable error ", error);
+    }
+  };
+  const [monthTT, setMonthTT] = useState(() =>
+    Array.from({ length: 30 }, () => [])
+  );
+
+  const onMonthChange = (direction) => {
+    setCalendarState((prevState) => {
+      const newStartDate = new Date(prevState.startDate);
+      newStartDate.setMonth(newStartDate.getMonth() + direction);
+      return createCalendarData(newStartDate);
+    });
+  };
+
+  useEffect(() => {
+    getTimeTables();
+  }, [calendarState]);
+
   return (
-    <NoticeSection>
-      <NoticeWrapper>
-        <h2>CALENDAR</h2>
-      </NoticeWrapper>
-    </NoticeSection>
+    <CalendarSection>
+      <CalendarWrapper>
+        <h1>CALENDAR</h1>
+
+        <MonthYearContainer>
+          <button onClick={() => onMonthChange(-1)}>
+            <img src="/assets/rightarrow_blue.svg" className="down" />
+          </button>
+          <h2>
+            {months[calendarState.startDate.getMonth()]},{" "}
+            {calendarState.startDate.getFullYear()}
+          </h2>
+          <button onClick={() => onMonthChange(1)}>
+            <img src="/assets/rightarrow_blue.svg" className="up" />
+          </button>
+        </MonthYearContainer>
+
+        <GridContainer>
+          {Array.from({ length: col }, (_, index) => {
+            return <WeekDayCell key={index}> {days[index]} </WeekDayCell>;
+          })}
+          {Array.from({ length: col * row }, (_, index) => {
+            return (
+              <DateCell
+                key={index}
+                dateObj={calendarState.dates[index]}
+                timetables={monthTT[index]}
+                isSameMonth={
+                  calendarState.dates[index].getMonth() ===
+                  calendarState.startDate.getMonth()
+                }
+              />
+            );
+          })}
+        </GridContainer>
+      </CalendarWrapper>
+    </CalendarSection>
   );
 };
 
---
+++ frontend/src/pages/calendar/DraggableTable.jsx
@@ -0,0 +1,326 @@
+import { React, useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
+import _ from "lodash";
+import { TTColors } from "../../data/CalendarData";
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(${(props) => props.$length}, 2rem);
+  grid-template-columns: 20rem;
+  border: solid 1px black;
+  /* overflow: hidden; */
+`;
+
+const TableCell = styled.div`
+  /* box-sizing: border-box; */
+  cursor: pointer;
+
+  color: #486284;
+  font-family: Pretendard;
+  font-size: 0.9rem;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  letter-spacing: -1.25px;
+
+  overflow: hidden;
+  text-overflow: clip;
+  border-bottom: solid 0.5px #acbeff;
+
+  background-color: ${(props) => props.$color};
+
+  user-select: none;
+`;
+
+const TTTitleContainer = styled.div`
+  position: absolute;
+  top: 0;
+  display: grid;
+  grid-template-columns: 20rem;
+  border: solid 1px black;
+
+  background: transparent;
+  pointer-events: none;
+`;
+
+const TTTCell = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  text-overflow: clip;
+
+  background: transparent;
+  pointer-events: none;
+
+  /* 마지막 요소는 오른쪽 보더 제거 */
+  &:last-child {
+    border-right: none;
+  }
+`;
+
+const TTTInnerBlock = styled.div`
+  padding: 10px;
+  margin-left: 1.5rem;
+  width: 60%;
+  height: 60%;
+
+  pointer-events: ${(props) => (props.$isTouched === -1 ? "auto" : "none")};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  border-radius: 2px;
+  background-color: ${(props) => props.$color1};
+  color: ${(props) => props.$color2};
+
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  letter-spacing: -0.9px;
+`;
+const Wrapper = styled.div`
+  position: relative;
+`;
+const DraggableTable = ({
+  times, // 시간들
+  cells,
+  setCells,
+  timeTables, // 해당 일(날짜)의 타임테이블 데이터들
+  setSelectedTT, // 현재 선택된 타임테이블
+  enableChange = false, // 변경 가능 여부
+}) => {
+  const [isTouched, setIsTouched] = useState(-1);
+
+  const [TTTCells, setTTTCells] = useState([]);
+  const [TTTStyle, setTTTStyle] = useState("");
+
+  const clearCells = () => {
+    let newCells = [...cells];
+    for (let i = 0; i < times.length; i++) {
+      newCells[i] = {
+        ...newCells[i],
+        isSelected: false,
+      };
+    }
+    setCells(newCells);
+  };
+  useEffect(clearCells, []);
+
+  // cells를 채워주는 useEffect
+  const initTable = useEffect(() => {
+    for (let i = 0; i < timeTables.data.length; i++) {
+      const parseTime = (str) => {
+        const [datePart, timePart] = str.split("T");
+        const [year, month, day] = datePart.split("-").map(Number);
+        const [hour, minute] = timePart.split(":").map(Number);
+        return (hour * 60 + minute - 9 * 60) / 30;
+      };
+
+      const startidx = parseTime(timeTables.data[i].start);
+      const endidx = parseTime(timeTables.data[i].end);
+
+      setCells((prevSelected) => {
+        const newSelected = [...prevSelected];
+
+        for (let j = startidx; j < endidx; j++) {
+          newSelected[j] = {
+            ...newSelected[j],
+            tt: timeTables.data[i],
+          };
+        }
+        return newSelected;
+      });
+    }
+  }, [timeTables]);
+
+  // 테이블 위에 표시되는 팀 이름을 표시해주는 useEffect
+  useEffect(() => {
+    let prev = null;
+    let cnt = 0;
+    let cssstr = "";
+    let cellData = [];
+
+    for (let i = 0; i < times.length; i++) {
+      if (i === 0 || cells[i].tt === prev) cnt++;
+      else {
+        cssstr += String(cnt * 2) + "rem ";
+
+        if (prev === null) cellData.push(null);
+        else cellData.push({ ...prev });
+        cnt = 1;
+      }
+      prev = cells[i].tt;
+    }
+    if (prev === null) cellData.push(null);
+    else cellData.push({ ...prev });
+    cssstr += String(cnt * 2) + "rem ";
+    setTTTStyle(cssstr);
+    setTTTCells(cellData);
+  }, [cells]);
+
+  const handleTouchStart = useCallback((e) => {
+    // if (isTouched != -1) return;
+
+    // 표시하는 일정 없애버리기
+    setSelectedTT(null);
+    const target = e.target.closest("div[data-key]");
+    if (!target) return;
+
+    const col = parseInt(target.dataset.key, 10);
+
+    setCells((prevSelected) => {
+      const newSelected = [...prevSelected];
+
+      // 처음 눌렀을 때 초기화 하는 동작을 없애려면 아래 블록 주석처리.
+      for (let i = 0; i < times.length; i++) {
+        newSelected[i] = {
+          ...newSelected[i],
+          isSelected: false,
+        };
+      }
+      //
+      newSelected[col] = {
+        ...newSelected[col],
+        isSelected: !newSelected[col].isSelected,
+      };
+      return newSelected;
+    });
+    setIsTouched(col);
+    console.log("start");
+  }, []);
+
+  const handleTouchEnd = useCallback(() => {
+    console.log("end");
+
+    setIsTouched(-1);
+  }, []);
+
+  const handleTouchMove = useCallback(
+    _.throttle((e) => {
+      if (isTouched === -1) return;
+
+      const clientX = e.type.startsWith("touch")
+        ? e.touches[0].clientX
+        : e.clientX;
+      const clientY = e.type.startsWith("touch")
+        ? e.touches[0].clientY
+        : e.clientY;
+      const target = document.elementFromPoint(clientX, clientY);
+
+      const st_col = isTouched;
+
+      let add_dates = true;
+      if (cells[st_col].isSelected === false) add_dates = false;
+
+      if (target.getAttribute("data-key") == null) return;
+      const cur_col = +target.getAttribute("data-key");
+
+      // console.log(cur_col, lastTouchedCol)
+      //////////
+
+      //   console.log(cells);
+      let newSelected = [...cells];
+
+      const minCol = Math.min(st_col, cur_col);
+      const maxCol = Math.max(st_col, cur_col);
+
+      for (let c = minCol; c <= maxCol; c++) {
+        if (c === st_col) continue;
+        newSelected[c] = { ...newSelected[c], isSelected: add_dates };
+      }
+
+      setCells(newSelected);
+
+      //   console.log(newSelected);
+    }, 75),
+    [isTouched]
+  );
+  useEffect(() => {
+    const preventScroll = (event) => event.preventDefault();
+
+    if (isTouched !== -1) {
+      document.addEventListener("touchmove", preventScroll, { passive: false });
+      document.addEventListener("touchmove", handleTouchMove);
+      document.addEventListener("mousemove", handleTouchMove);
+      //   document.addEventListener("pointerup", handleTouchEnd);
+    } else {
+      document.removeEventListener("touchmove", preventScroll);
+      document.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("mousemove", handleTouchMove);
+      //   document.removeEventListener("pointerup", handleTouchEnd);
+    }
+
+    return () => {
+      document.removeEventListener("touchmove", preventScroll);
+      document.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("mousemove", handleTouchMove);
+      //   document.removeEventListener("pointerup", handleTouchEnd);
+    };
+  }, [isTouched]);
+  return (
+    <Wrapper>
+      <GridContainer
+        onPointerDown={enableChange ? handleTouchStart : undefined}
+        onPointerUp={enableChange ? handleTouchEnd : undefined}
+        $length={times.length}
+      >
+        {times.map((time, index) => (
+          <TableCell
+            key={index}
+            data-key={index}
+            $color={
+              cells[index]?.isSelected === false ? "white" : "#FFF7E2;"
+              // : cells[index] === 1
+              // ? "green"
+              // : cells[index]?.color ?? "white"
+            }
+          >
+            {String(time.getHours()).padStart(2, "0")}:
+            {String(time.getMinutes()).padStart(2, "0")}
+          </TableCell>
+        ))}
+      </GridContainer>
+      <TTTitleContainer
+        id="TTTitleContainer"
+        style={{
+          gridTemplateRows: TTTStyle,
+        }}
+      >
+        {TTTCells.map((cell, index) => (
+          <TTTCell key={index}>
+            {TTTCells[index] !== null && (
+              <TTTInnerBlock
+                onClick={() => {
+                  if (enableChange) clearCells();
+                  return setSelectedTT(TTTCells[index]);
+                }}
+                $color1={
+                  TTColors[parseInt(TTTCells[index].color)]?.[0] ??
+                  TTColors[0][0]
+                }
+                $color2={
+                  TTColors[parseInt(TTTCells[index].color)]?.[1] ??
+                  TTColors[0][1]
+                }
+                $isTouched={isTouched}
+              >
+                <div>
+                  {TTTCells[index] === null ? null : TTTCells[index].title}
+                </div>
+                <div>
+                  {TTTCells[index] === null ? null : TTTCells[index].team}
+                </div>
+              </TTTInnerBlock>
+            )}
+          </TTTCell>
+        ))}
+      </TTTitleContainer>
+    </Wrapper>
+  );
+};
+export default DraggableTable;
