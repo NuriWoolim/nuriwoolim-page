1582115e8365c41d06619faf3ac683daf625cb58
minh811 <minhwang03@gmail.com>
2025-10-29 18:53:07 +0900

    Address review comments

--- backend/src/main/java/com/nuriwoolim/pagebackend/domain/board/repository/BoardRepository.java
+++ backend/src/main/java/com/nuriwoolim/pagebackend/domain/board/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package com.nuriwoolim.pagebackend.domain.board.repository;
 
 import com.nuriwoolim.pagebackend.domain.board.entity.Board;
-<<<<<<< HEAD
 import com.nuriwoolim.pagebackend.domain.calendar.entity.Calendar;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,9 +10,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
->>>>>>> 488027a (feat: 게시판 CRUD 구현)
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     boolean existsByTitle(String title);
